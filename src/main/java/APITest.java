0379eb10fb8ecaf14928fce09ac275f84b47110f
mengwang <mengw3@illinois.edu>
2018-03-28 19:27:12 -0500

    changed

--- src/main/java/APITest.java
+++ src/main/java/APITest.java
@@ -16,7 +16,7 @@ public class APITest {
 
     /** Subscription key. Replace with your API key. */
     private static final String SUBSCRIPTION_KEY =
-            "13hc77781f7e4b19b5fcdd72a8df7156";
+            "40e988d65a28469e8e95f99ca614099e";
 
     /**
      * Region to use.
